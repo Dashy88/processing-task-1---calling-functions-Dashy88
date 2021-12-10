3d6f04549d79ba68774572611af80f72dd709dc1
Dashy88 <95883387+Dashy88@users.noreply.github.com>
2021-12-10 14:56:15 -0500

    commit

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
 import processing.core.PApplet;
 
-/**
- * Main class to execute sketch
- * @author 
- *
- */
+
 class Main {
-  public static void main(String[] args) {
-    
-    String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch();
-	  PApplet.runSketch(processingArgs, mySketch);
-  }
-  
+    public static void main(String[] args) {
+        //PApplet.main("Sketch", args);
+
+        String[] processingArgs = {"MySketch"};
+        Sketch mySketch = new Sketch();
+        PApplet.runSketch(processingArgs, mySketch);
+    }
 }
